cfcf718761d088da9a434d83d18e2c9b4cf82382
tomoron <tomoron326@outlook.jp>
2022-06-07 20:59:08 +0900

    add:insertItems

--- utils/http.js
+++ utils/http.js
@@ -1,4 +1,4 @@
-const fetchItems = async (url) => {
+export const fetchItems = async (url) => {
   try {
     const response = await fetch(url);
     return await response.json();
@@ -6,6 +6,19 @@ const fetchItems = async (url) => {
     console.error("データが取得できませんでした");
     console.error(error);
   }
+};
+
+export const insertItems = async (url, data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  }).catch(() => {
+    console.error(response.json());
+    return;
+  });
 };
 
 export default fetchItems;
