4f095c3f2d0238b49e527d63fe78f84bc4f0d4e1
tomoron <tomoron326@outlook.jp>
2022-06-07 19:22:10 +0900

    add:board

---
+++ src/components/MainBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class = 'board-style'>
+    <MainList
+      v-for="category in categoryList"
+      :key="category.id"
+      :category="category"
+    ></MainList>
+  </div>
+</template>
+
+
+<script>
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+import MainList from './MainList.vue';
+
+export default {
+  components: {MainList},
+  setup() {
+    const store = useStore();
+    onMounted(store.dispatch("fetchCategoryList"));
+    return {
+      categoryList: computed(() => store.state.categoryList)
+    };
+  }
+}
+
+</script>
+
+<style scoped>
+.board-style {
+  display: flex;
+  gap: 10px;
+}
+</style>
