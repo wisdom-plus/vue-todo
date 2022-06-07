72307dd6d1183c4ea9df84718afffaf68e531690
tomoron <tomoron326@outlook.jp>
2022-06-07 19:23:25 +0900

    pref:app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
   <div>
-    カード一覧：{{ cardList }}<br />
-    カテゴリ一覧：{{ categoryList }}
+    <MainBoard></MainBoard>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from "vue";
-import { useStore } from "vuex";
+import MainBoard from './components/MainBoard.vue';
 
 export default {
   name: "App",
-  components: {},
-  setup() {
-    const store = useStore();
-    onMounted(store.dispatch("fetchCategoryList"));
-    onMounted(store.dispatch("fetchCardList"));
-    return {
-      cardList: computed(() => store.state.cardList),
-      categoryList: computed(() => store.state.categoryList)
-    };
-  }
+  components: { MainBoard }
 };
 </script>
 
