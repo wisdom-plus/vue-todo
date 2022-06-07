041dc5686a7a73e0aa101256204107fe92988a54
tomoron <tomoron326@outlook.jp>
2022-06-07 19:22:39 +0900

    add:Card

---
+++ src/components/MainCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-style">
+    {{card.text}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object
+  }
+};
+</script>
+
+
+<style scoped>
+.card-style {
+  display: flex;
+  flex-direction: column;
+  background-color: yellowgreen;
+  margin: 10px;
+  height: 10vh;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+}
+</style>
