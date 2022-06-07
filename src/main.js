ac41780894dadda724e9fac252be30396e4d24ab
tomoron <tomoron326@outlook.jp>
2022-06-07 16:54:25 +0900

    add:use(Store)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import Store from "./store/store.js";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).use(Store).mount("#app");
